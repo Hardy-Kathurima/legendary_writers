ab4ffaaf7fb59d2ffc32eb35efcbf368e7c4261c
Hardy <hardykathurima@gmail.com>
2022-04-02 16:49:35 +0300

    added tawkto

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -172,6 +172,19 @@
   toastr.success("Email sent successfully!");
     });
         </script>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6248502f2abe5b455fc39214/1fvl7iepd';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+        </script>
+        <!--End of Tawk.to Script-->
 </body>
 
 </html>
